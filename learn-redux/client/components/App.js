32da9ade1cfabcfc158a8b409d88ea7fa59afddc
Robert-Jung <rjinjung@gmail.com>
2017-05-26 14:37:51 -0700

    Completed exercise 11

---
+++ learn-redux/client/components/App.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as ActionCreators from '../actions/ActionCreators'
+import Main from './Main'
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(ActionCreators, dispatch)
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main)
+
+export default App
